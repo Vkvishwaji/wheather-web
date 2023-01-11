566187ee7625340415ab3cbd53c1ea56cc0914a6
vikas <vkvishwaji@gmail.com>
2023-01-11 23:11:00 +0530

    my weather project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Temp from './component/wether/Temp';
+
+function App() {
+  return (
+     <>
+<Temp />
+     </>
+  )
+}
+
+export default App;
